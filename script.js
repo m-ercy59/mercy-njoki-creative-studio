6d32e9b6e34673cd81418abe7b2ed40e3da8655c
m-ercy59 <mnjokin22@gmail.com>
2026-07-07 15:37:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -238,27 +238,7 @@ if (form) {
 }
 
 
-// ==========================
-// Loader
-// ==========================
 
-window.addEventListener("load", () => {
-
-    const loader = document.getElementById("loader");
-
-    if (loader) {
-
-        loader.style.opacity = "0";
-
-        setTimeout(() => {
-
-            loader.style.display = "none";
-
-        }, 600);
-
-    }
-
-});
 
 
 // ==========================
